b78db2fdfea045b7238049ccf66f81744f6a542b
DSharma817 <dheerajrocks2010@gmail.com>
2017-03-26 11:17:35 -0400

    Create carrots.java

---
+++ carrots.java
@@ -0,0 +1,15 @@
+/* 
+ * @author Dheeraj Sharma
+ * @version 1.0
+ */
+ 
+import java.util.Scanner;
+public class solvingForCarrots {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        int N = input.nextInt();
+        int P = input.nextInt();
+        System.out.println(P);
+        input.close();
+    }
+}
